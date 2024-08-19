c24a3cceaa043c1282721d40995ab9e0c24ad4f9
riishabhraj <rishabhraj5421@gmail.com>
2024-08-19 15:12:08 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("this is DevOps World"));
+app.get("/", (req, res) => res.send("Welcoming Rishabh to the DevOps World"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
